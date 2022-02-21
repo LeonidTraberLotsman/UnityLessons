c49cda64014f49440585667a0cbe13cb76dbe66c
lotsman-12 <leonidt@lotsman.org>
2022-02-21 20:02:11 +0500

    Создан префаб врага

--- test1/test1/Assets/Scripts/Enemy.cs
+++ test1/test1/Assets/Scripts/Enemy.cs
@@ -1,24 +1,34 @@
+using UnityEngine.AI;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public Transform Head;
+    public Transform player;
+    NavMeshAgent    agent;
     int a=5;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+    if(a>0){
+        agent.destination=player.position;
+    }
+    
         
     }
     
     public void Die()
     {
+        agent.enabled=false;
+        Head.GetComponent<Rigidbody>().isKinematic=false;
         GetComponent<Rigidbody>().isKinematic=false;
 
 
